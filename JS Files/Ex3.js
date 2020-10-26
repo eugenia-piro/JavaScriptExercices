7e0d40abce43531aec67b49743202427f5b5264d
Maria Eugenia Piro <piromariaeugena@gmail.com>
2020-10-26 12:22:22 -0300

    Exercise3

--- JS Files/Ex3.js
+++ JS Files/Ex3.js
@@ -1,4 +1,42 @@
 //Exercise 3 - Arrays
 /* Part a:
-Crear una variable de tipo string con al menos 10 caracteres y convertir todo 
-el texto en mayúscula (utilizar toUpperCase).*/
+Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", 
+"Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] 
+mostrar por consola los meses 5 y 11 (utilizar console.log).*/
+var months = [
+    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", 
+    "Septiembre", "Octubre", "Noviembre", "Diciembre"
+];
+console.log(months[5],months[11]);
+/* Part b:
+Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar 
+sort).*/
+months.sort();
+console.log(months);
+/* Part c:
+Agregar un elemento al principio y al final del array (utilizar unshift y push).
+*/
+months.unshift('Al principio');
+months.push('Al final');
+console.log(months);
+/* Part d:
+Quitar un elemento del principio y del final del array (utilizar shift y pop).*/
+months.shift();
+months.pop();
+console.log(months);
+/* Part e:
+Invertir el orden del array (utilizar reverse).*/
+months.reverse();
+console.log(months);
+/* Part f:
+Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).*/
+var f = months.join('-');
+console.log(f);
+/* Part g:
+Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).*/
+var months = [
+    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", 
+    "Septiembre", "Octubre", "Noviembre", "Diciembre"
+];
+var monthsCopy = months.slice(4,11);
+console.log(monthsCopy);
